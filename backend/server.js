37eac6add28d302c0f5d82551ad9f73aef17626e
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2024-12-05 11:48:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'https://aeshthreetstshirts.vercel.app' // Frontend URL, adjust in production
+    origin: 'https://aeshthreets-tshirts.onrender.com' // Frontend URL, adjust in production
 }));
 
 // MongoDB Atlas Connection
