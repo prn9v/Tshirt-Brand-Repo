2feb6e4acbfa4fa4388cf26d3f09a8e7a5942a11
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2024-12-06 17:13:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(bodyParser.json()); // Parse JSON bodies
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000', // Use env variable for frontend URL
+    origin: 'https://aeshthreets-tshirts.onrender.com', // Use env variable for frontend URL
 }));
 
 // MongoDB Atlas Connection
