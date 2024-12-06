7406c682cc84b70c9663d5fa1589b8d2a75fa4dd
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2024-12-06 16:57:41 +0530

    Update Product.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,60 +1,19 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    productName: { 
-        type: String, 
-        required: [true, "Product name is required"] 
-    },
-    productDescription: { 
-        type: String 
-    },
-    productPrice: { 
-        type: Number, 
-        required: [true, "Product price is required"],
-        min: [0, "Product price must be a positive number"]
-    },
-    productSizes: { 
-        type: [String], 
-        required: [true, "Product sizes are required"],
-        validate: {
-            validator: (sizes) => sizes.length > 0,
-            message: "At least one size must be provided"
-        }
-    }, // Example: ['S', 'M', 'L', 'XL']
-    productColors: { 
-        type: [String], 
-        required: [true, "Product colors are required"],
-        validate: {
-            validator: (colors) => colors.length > 0,
-            message: "At least one color must be provided"
-        }
-    }, // Example: ['red', 'blue', 'green']
-    // productImage: { 
-    //     data: Buffer, 
-    //     contentType: String 
-    // },
-    productDiscount: { 
-        type: Number, 
-        required: [true, "Product discount is required"],
-        min: [0, "Product discount must be a positive number"]
-    },
-    productFinalPrice: { 
-        type: Number, 
-        required: [true, "Product final price is required"],
-        min: [0, "Product final price must be a positive number"]
-    },
-    productGender: { 
-        type: String, 
-        required: [true, "Product gender is required"],
-        enum: ["Male", "Female", "Unisex"], // Optional: restrict values
-        default: "Unisex"
-    },
-    productType: { 
-        type: String, 
-        required: [true, "Product type is required"]
-    },
-}, { timestamps: true }); // Add timestamps for createdAt and updatedAt fields
+  productName: { type: String, required: true },
+  productSizes: { type: [String], required: true },
+  productColors: { type: [String], required: true },
+  productPrice: { type: Number, required: true },
+  productDiscount: { type: Number },
+  productFinalPrice: { type: Number, required: true },
+  productDescription: { type: String, required: true },
+  productGender: { type: String, required: true },
+  productType: { type: String, required: true },
+  productImage: { 
+      data: Buffer, 
+      contentType: String 
+  },
+});
 
-const Product = mongoose.model('Product', productSchema);
-
-module.exports = Product;
+module.exports = mongoose.model('Product', productSchema);
