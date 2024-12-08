20c9a75c46970f0b48defff07384a79ddcf482af
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2024-12-08 19:19:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 
 // Load environment variables
 dotenv.config();
@@ -16,18 +17,18 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 const allowedOrigins = [
     'http://localhost:3000',  // Development
-    'https://aeshthreets-tshirts.onrender.com'  // Production
-  ];
-  
-  app.use(cors({
+    'https://aeshthreets-tshirts.onrender.com',  // Production
+];
+
+app.use(cors({
     origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) { // Allow server-side requests too
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    }
-  }));
+        if (allowedOrigins.includes(origin) || !origin) { // Allow server-side requests too
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+}));
 
 // MongoDB Atlas Connection
 const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://pranavdeshmukh5454:5DUxv2OyPWWdBAwk@cluster0.4snwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
@@ -36,17 +37,20 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected successfully'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
-// Routes
-const productRoutes = require('./routes/productRoutes');
-app.use('/api/products', productRoutes);
 
 // Static Files for Uploads
 const uploadsDir = path.join(__dirname, 'uploads');
-
 if (!fs.existsSync(uploadsDir)) {
     fs.mkdirSync(uploadsDir);
     console.log('Uploads folder created!');
 }
+
+// Routes
+const productRoutes = require('./routes/productRoutes');
+app.use('/api/products', productRoutes);
+
+
+
 app.use('/uploads', express.static(uploadsDir));
 
 // Root Route
