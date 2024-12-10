bfbda6cec1578e4c5d04ca27e95f9ff29bd35152
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2024-12-10 23:55:15 +0530

    Update productApi.js

--- frontend/src/api/productApi.js
+++ frontend/src/api/productApi.js
@@ -4,7 +4,7 @@ const API_URL = 'http://localhost:5000/api/products'; // Backend API URL
 
 // Add a new product (for admin use)
 export const addProduct = async (productData) => {
-    const API_URL = '`${API_URL}/add`'; // Replace with your actual backend URL
+    const API = '`${API_URL}/add`'; // Replace with your actual backend URL
     const config = {
     headers: {
       'Content-Type': 'multipart/form-data', // Important for FormData
@@ -14,7 +14,7 @@ export const addProduct = async (productData) => {
   try {
     console.log('Sending product data to backend:', productData);
 
-    const response = await axios.post(API_URL, productData, config);
+    const response = await axios.post(API, productData, config);
 
     console.log('Product successfully added:', response.data);
     return response.data; // Return the server's response
