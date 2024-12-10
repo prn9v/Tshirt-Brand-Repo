a65703ef0b962527f885937d6f47fe4160b936d0
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2024-12-10 23:59:11 +0530

    Update productApi.js

--- frontend/src/api/productApi.js
+++ frontend/src/api/productApi.js
@@ -4,7 +4,7 @@ const API_URL = 'http://localhost:5000/api/products'; // Backend API URL
 
 // Add a new product (for admin use)
 export const addProduct = async (productData) => {
-    const API = '`${API_URL}/add`'; // Replace with your actual backend URL
+    const API = `${API_URL}/add`; // Replace with your actual backend URL
     const config = {
     headers: {
       'Content-Type': 'multipart/form-data', // Important for FormData
