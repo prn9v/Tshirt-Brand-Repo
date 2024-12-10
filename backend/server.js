b323498d0904cc643c9f9d4fde1b1f5f87ddff1e
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2024-12-10 22:55:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 const allowedOrigins = [
     'http://localhost:3000',  // Development
-    'https://aeshthreets-tshirts.onrender.com',  // Production
+    'https://aeshthreets-tshirts.onrender.com/',  // Production
 ];
 
 app.use(cors({
