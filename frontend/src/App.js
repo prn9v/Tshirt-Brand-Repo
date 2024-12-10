12213bd25b4f04e3e2409baeb816738ee5811fc7
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2024-12-11 00:46:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Import your components/pages
@@ -12,23 +12,33 @@ import Products from "./components/Products";
 import MenProduct from "./components/MenProduct";
 import WomenProduct from "./components/WomenProduct";
 import ProductCard from "./components/ProductCard";
+import PrivateRoute from "./components/PrivateRoute"; // Import the PrivateRoute component
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <Router>
       <div className="App">
-        <Navbar/>
+        <Navbar />
         <Routes>
-            <Route path="/" element={<LoginPage />} /> 
-            <Route path="/home" element={<HomePage />} />  
-            <Route path="/about" element={<AboutUs />} /> 
-            <Route path="/admin" element={<AdminPage />} />
-            <Route path="/products" element={<Products/>}/>
-            <Route path="/products/men" element={<MenProduct/>}/>
-            <Route path="/products/women" element={<WomenProduct/>}/>
-            <Route path="/products/:productId" element={<ProductCard />}/>
+          <Route path="/" element={<LoginPage setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute isAuthenticated={isAuthenticated}>
+                <AdminPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/men" element={<MenProduct />} />
+          <Route path="/products/women" element={<WomenProduct />} />
+          <Route path="/products/:productId" element={<ProductCard />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     </Router>
   );
